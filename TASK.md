1fefb59b2993310413828be5355730e3313bdb83
xenobiont <61256244+xenobiont@users.noreply.github.com>
2024-01-29 02:50:36 +0200

    Initial commit

---
+++ TASK.md
@@ -0,0 +1,12 @@
+# Task
+
+You have a clock component.
+
+1. Make display of seconds optional and controlled from app component;
+
+2. Change the clock component to receive time value from app component (it still should be periodically updated);
+
+3. Add a second clock.
+   Make each clock to display different icon between minutes and seconds (use svg files from assets directory)
+
+4. Add a button to each clock, that will stop all the clocks if pressed.
---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-clock',
+	template: `<p>{{ hour }}:{{ min }}:{{ sec }}</p>`,
+})
+export class ClockComponent {
+	hour: number = 0;
+	min: number = 0;
+	sec: number = 0;
+
+	constructor() {
+		setInterval(() => this.updateTime(), 1000);
+	}
+
+	updateTime() {
+		const time = new Date();
+		this.hour = time.getHours();
+		this.min = time.getMinutes();
+		this.sec = time.getSeconds();
+	}
+}
